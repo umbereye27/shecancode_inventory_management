e8432177dbc19d952cbd441cd53657eab1b0180f
UWIMPAYE Yvette <nkusiyvette1@gmail.com>
2024-01-17 11:59:38 +0200

    Settings

---
+++ dark/src/Theme.jsx
@@ -0,0 +1,16 @@
+// Theme.jsx
+
+export const lightTheme = {
+    body: '#ffffff',
+    text: '#363537',
+    toggleBorder: '#FFF',
+    background: '#363537',
+  };
+  
+  export const darkTheme = {
+    body: '#363537',
+    text: '#FAFAFA',
+    toggleBorder: '#6B8096',
+    background: '#999',
+  };
+  
